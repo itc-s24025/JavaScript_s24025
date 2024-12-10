07ad5a35f92b7d22d060da38a823fa6b7ca4d84a
julia <s24025@std.it-college.ac.jp>
2024-12-10 16:09:47 +0900

    現在時刻を表示

---
+++ CH05/CH05-04/script.js
@@ -0,0 +1,18 @@
+var time_el = document.getElementById('time_el');
+
+var date = new Date();
+var hour = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+time_el.innerHTML = hour+':'+min+':'+sec;
+
+setInterval(function(){
+    var time_el = document.getElementById('time_el');
+  
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    time_el.innerHTML = hour+':'+min+':'+sec;
+}, 1000);
+  
